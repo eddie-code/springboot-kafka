967ca3fef74212fca7f2f9eb6eaccb573c9d0de9
eddie-code <231071372@qq.com>
2020-10-09 17:31:55 +0800

    13-1-4 Kafka与springboot整合_生产者

---
+++ kafka-consumer/src/main/java/com/kafka/Application.java
@@ -0,0 +1,22 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author eddie.lee
+ * @projectName springboot-kafka
+ * @package com.kafka
+ * @className Application
+ * @description
+ * @date created in 2020-10-09 16:33
+ * @modified by
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
